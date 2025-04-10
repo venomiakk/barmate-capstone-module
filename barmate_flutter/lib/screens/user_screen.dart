427bdfaa35e920d7989083352f1f035aa35df1e8
FckPerv <78822364+FckPerv@users.noreply.github.com>
2025-04-10 22:28:18 +0200

    account jpage updated

--- barmate_flutter/lib/screens/user_screen.dart
+++ barmate_flutter/lib/screens/user_screen.dart
@@ -10,35 +10,102 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Center( 
+        child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start, 
-            crossAxisAlignment: CrossAxisAlignment.center, 
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              SizedBox(height: 20), // Adjust the height as needed
-              const Text(
-                'Account',
-                style: TextStyle(fontSize: 24),
-              ),
+              SizedBox(
+                height: size.height * 0.03,
+              ), // Adjust the height as needed
+              const Text('Account', style: TextStyle(fontSize: 24)),
+              SizedBox(height: size.height * 0.03),
               Container(
-              width: 80,
-              height: 80,
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
+                width: size.width * 0.9,
+                height: size.height * 0.1,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: userProfile(),
               ),
-              child: Image.network(
-                'https://picsum.photos/seed/picsum/200/300',
-                fit: BoxFit.cover,
+              SizedBox(height: size.height * 0.03),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+                child: Align(
+                  alignment:
+                      Alignment.centerLeft, // Aligns the text to the left
+                  child: Text(
+                    'My favorite drinks',
+                    textAlign:
+                        TextAlign.left, // Ensures the text is left-aligned
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               ),
-            ),
-              
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Row userProfile() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start, // Align items to the start
+      crossAxisAlignment: CrossAxisAlignment.center, // Vertically center items
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ), // Add some spacing
+          child: Container(
+            width: 80,
+            height: 80,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(shape: BoxShape.circle),
+            child: const Image(image: AssetImage('images/profile_picture.png')),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'User Name',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text('User title', style: TextStyle(fontSize: 18)),
+          ],
+        ),
+        const Spacer(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: IconButton(
+            icon: const Icon(Icons.settings, color: Colors.grey, size: 40),
+            onPressed: () {
+              // Add your edit profile action here
+            },
+          ),
+        ),
+
+        // Pushes the icon to the right
+      ],
     );
   }
 }
